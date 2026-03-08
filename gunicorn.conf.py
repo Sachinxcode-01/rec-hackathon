fff2393e8849c1fd2da8293c7479cd7002ddb5db
Sachinxcode-01 <kalinganvarsachin@gmail.com>
2026-03-08 09:10:40 +0530

    Update gunicorn to use eventlet worker for Socket.IO support

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,9 +3,9 @@ import os
 # Binding to the port Railway provides, defaulting to 8080 which is common for Nixpacks
 bind = "0.0.0.0:" + os.environ.get("PORT", "8080")
 
-# Stick with 1 worker during debugging to see all logs clearly
+# Using eventlet worker class for Socket.IO support
+worker_class = "eventlet"
 workers = 1
-threads = 4
 timeout = 300
 preload_app = False
 
